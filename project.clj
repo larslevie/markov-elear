ddf9f936b76ba105025893ee2e0bd5b3bc81c256
Lars Levie <larslevie@gmail.com>
2016-06-09 14:19:14 -0500

    Update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone/at-at "1.2.0"]
                  [twitter-api "0.7.8"]
                  [environ "1.0.0"]]
